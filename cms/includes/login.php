a430d5e3148fd771397e51e0a47b76d36b0cc2a7
Andres <andimier@gmail.com>
2020-11-24 13:56:50 -0500

    BE - refactor login

--- cms/includes/login.php
+++ cms/includes/login.php
@@ -1,68 +1,98 @@
-<?php 
-
-	if(!isset($_SESSION)){
+<?php
+	if (!isset($_SESSION)) {
 		session_start();
-	} 
-	
+	}
+
 	$mensaje = "";
 	$mensaje1 = "";
 	$mensaje2 = "";
 	$mensaje3 = "";
 	$mensaje4 = "";
-		
+
 	require_once("includes/connection.php");
+	require_once("../utils/phpfunctions.php");
 	require_once("includes/functions.php");
-	//
- 	//
-	if(isset($_POST['submit'])){
-		$errores = array();
-		$required_fields = array('usuario','contrasena');
-		//$errores = array_merge($errores, $required_fields);
-		//$errores = array_merge($errores, check_required_fields($required_fields, $_POST));
+
+	function getUser($username, $hashed_password) {
+		$q = "SELECT id, username FROM usuarios WHERE username = '{$username}' AND hashed_password = '{$hashed_password}'";
+		$r = phpMethods('query', $q);
+
+		confirm_query($r);
+
+		if ($r == null || phpMethods('num-rows', $r) == 0) {
+			return null;
+		}
+
+		return $r;
+	}
+
+	function getErrors() {
+		$errors = array();
+		$required_fields = [
+			'usuario',
+			'contrasena'
+		];
+
+		//$errors = array_merge($errors, $required_fields);
+		//$errors = array_merge($errors, check_required_fields($required_fields, $_POST));
+
 		foreach($required_fields as $fieldname){
 			if(!isset($_POST[$fieldname]) || (empty($_POST[$fieldname])  && !is_numeric($_POST[$fieldname]))){
-				$errores[] = $fieldname;	
+				$errors[] = $fieldname;
 			}
 		}
-		//
-		$username = trim(mysql_prep($_POST['usuario']));	
-		$password = trim(mysql_prep($_POST['contrasena']));	
+
+		return $errors;
+	}
+
+	function getHash($password) {
 		//algoritmos de incriptacion
 		//$hashed_password = md5($password);
 		//$hashed_password = hash($password);
-		$hashed_password = sha1($password);
-		//
-		if(empty($errores)){
-			$query = "SELECT id, username FROM usuarios WHERE username = '{$username}' AND hashed_password = '{$hashed_password}'";
-			$result = mysql_query($query, $connection);
-			confirm_query($result);
-			//
-			if(mysql_num_rows($result) == 1){
-				$usuario_encontrado = mysql_fetch_array($result);
-				$_SESSION['user_id'] = $usuario_encontrado['id'];
-				$_SESSION['username'] = $usuario_encontrado['username'];
-				header("Location: noticias.php");
-				exit;
-			}else{
-				$mensaje1 = "El nombre de usuario o contraseña pueden estar errados.";
-			}
-			
-		}else{
-			$mensaje2 = "Por favor ingresa los siguientes campos:<br />";
-			foreach($errores as $error){
-				$mensaje3 = " <li> " . $error . "</li><br/>";
-			} 
-			if(count($errores) ==1){
-				 $mensaje4 = "Hubo un error en el formulario.<br /><br />";
-			}else{
-				 $mensaje4 = "Hubo " . count($errores) . " errores en el formulario.<br /><br />" ;
-			}
-		}
-	}else{
-		if(isset($_GET['logout']) && $_GET['logout'] == 1){
-			$mensaje = "Has cerrado tu sesión. "	;
-		}
-		$username = "";	
-		$password = "";	
+		return sha1($password);
 	}
+
+	if (!isset($_POST['submit'])) {
+		if (isset($_GET['logout']) && $_GET['logout'] == 1) {
+			$mensaje = "Has cerrado tu sesiÃ³n. "	;
+		}
+
+		$username = "";
+		$password = "";
+
+		return;
+	}
+
+	if (!empty(getErrors())) {
+		$mensaje2 = "Por favor ingresa los siguientes campos:<br />";
+
+		foreach($errors as $error) {
+			$mensaje3 = " <li> " . $error . "</li><br/>";
+		}
+
+		if (count($errors) ==1) {
+			$mensaje4 = "Hubo un error en el formulario.<br /><br />";
+		} else {
+			$mensaje4 = "Hubo " . count($errors) . " errors en el formulario.<br /><br />" ;
+		}
+
+		return;
+	}
+
+	$username = trim(mysql_prep($_POST['usuario']));
+	$password = trim(mysql_prep($_POST['contrasena']));
+	$hashed_password = getHash($password);
+	$user = getUser($username, $hashed_password);
+
+	if ($user == null) {
+		$mensaje1 = "El nombre de usuario o contraseÃ±a pueden estar errados.";
+		return;
+	}
+
+	$usuario_encontrado = phpMethods('fetch-array',$user);
+	$_SESSION['user_id'] = $usuario_encontrado['id'];
+	$_SESSION['username'] = $usuario_encontrado['username'];
+
+	header("Location: noticias.php");
+	exit;
 ?>
--- cms/login.php
+++ cms/login.php
@@ -1,67 +1,46 @@
-<?php 
-require_once("includes/login.php");
-require_once("includes/functions.php");
+<?php
+	require_once("includes/login.php");
+	require_once("../utils/phpfunctions.php");
+	require_once("includes/functions.php");
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 	<head>
 		<?php include_once('includes/tags.php'); ?>
 	</head>
 
-	
 	<body>
-		
-		
-			
-			
-			
-			
-			<div id="cnt_login">
-			
-				<div id="mensaje_login">
-					<?php 
-						echo $mensaje;
-						echo $mensaje4;
-						echo $mensaje1;
-						echo $mensaje2;
-						echo $mensaje3;
-					?>
-				</div>
-
-				<form id="frm-login" action="login.php" method="post">
-				
-					<label for="campo_usuario" id="labe">Nombre de Usuario</label>
-					<input type="text" name="usuario" id="campo_usuario"  />
-					
-					<label for="password" id="label">Tu Contraseña</label>
-					<input type="password" name="contrasena" id="password" maxlength="50" />
-					
-					<br />
-					<br />
-					<br />
-					<input type="submit" name="submit" class="boton_entrar fondo_verde" value="Ingresar" />
-					
-					
-				</form>
-				
-				
+		<div id="cnt_login">
+			<div id="mensaje_login">
+				<?php
+					echo $mensaje;
+					echo $mensaje4;
+					echo $mensaje1;
+					echo $mensaje2;
+					echo $mensaje3;
+				?>
 			</div>
-			
-			
-			
-			
-		
-		
-		
-		
+
+			<form id="frm-login" action="login.php" method="post">
+				<label for="campo_usuario" id="labe">Nombre de Usuario</label>
+				<input type="text" name="usuario" id="campo_usuario"  />
+
+				<label for="password" id="label">Tu ContraseÃ±a</label>
+				<input type="password" name="contrasena" id="password" maxlength="50" />
+				<br />
+				<br />
+				<br />
+
+				<input type="submit" name="submit" class="boton_entrar fondo_verde" value="Ingresar" />
+			</form>
+		</div>
 		<div id="cabezote" ></div>
-		
 	</body>
 </html>
-    
+
 <?php
-	if(isset($connection)){
-		mysql_close($connection);
+	if (isset($connection)) {
+		phpMethods('close', $connection);
 	}
 ?>
