caa79c46d91b4f06e682a828a46c32887e15efb5
Andres <andimier@gmail.com>
2020-11-28 16:06:58 -0500

    BE - add functionality to remove entries

--- cms/crud/delete-entry.php
+++ cms/crud/delete-entry.php
@@ -5,40 +5,59 @@
 
     if (!isset($_POST['delete-entry']) ||
         !isset($_POST['id']) ||
-        !isset($_POST['table']) ||
-        !isset($_POST['image'])
-
+        !isset($_POST['table'])
     ) {
         echo 'no estamos borrando nada';
         return;
     }
 
     $id = $_POST['id'];
-    $tabla = $_POST['table'];
-    $image = $_POST['image'];
+    $table = $_POST['table'];
 
-    if (!empty($image) &&
-        $image != '../iconos/photo2.png' &&
-        $image != './iconos/photo.png' &&
-        $image != './iconos/default-image.png'
-    ) {
-        $explotarnombre = explode("/", $image);
-        $nombrearchivo  = $explotarnombre[2];
+    $can_delete_image = false;
+    $default_images = [
+        '../iconos/photo2.png',
+        './iconos/photo.png',
+        './iconos/default-image.png'
+    ];
 
-        $nombreA = "../imagenes/pequenas/" . $nombrearchivo;
-        $nombreB = "../imagenes/medianas/" . $nombrearchivo;
-        $nombreC = "../imagenes/grandes/"  . $nombrearchivo;
-
-        unlink($nombreA);
-        unlink($nombreB);
-        unlink($nombreC);
+    if (isset($_POST['image']) &&
+        !empty($_POST['image']) &&
+        !in_array($_POST['image'], $default_images))
+    {
+        $can_delete_image = true;
     }
 
+    if ($can_delete_image) {
+        $image = $_POST['image'];
+        $file_list = explode("/", $image);
+        $file_to_delete  = $file_list[count($file_list) - 1];
+
+        $small_image = "../imagenes/pequenas/" . $file_to_delete;
+        $mediuml_image = "../imagenes/medianas/" . $file_to_delete;
+        $large_image = "../imagenes/grandes/" . $file_to_delete;
+
+        unlink($small_image);
+        unlink($mediuml_image);
+        unlink($large_image);
+    }
+
+    $query = "DELETE FROM $table WHERE id = {$id} LIMIT 1";
+
     try {
-        $query = "DELETE FROM $tabla WHERE id = {$id} LIMIT 1";
         $result = phpMethods('query', $query);
-        header("Location: ../noticias.php");
     } catch (Exeptin $error) {
         echo $error;
     }
+
+    switch ($table) {
+		case 'noticias':
+			$location .= 'Location: ../noticias.php';
+			break;
+		case 'entries':
+			$location .= 'Location: ../edit-section.php';
+			break;
+	}
+
+    header($location);
 ?>
