2738f48b4ff3e78f3f76e90d8fa764e36c376872
Andres <andimier@gmail.com>
2020-12-05 19:14:33 -0500

    FE - change news page encoding

--- noticias.php
+++ noticias.php
@@ -11,7 +11,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=>UTF-8" />
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<title>Noticias - La Red Music</title>
 		<?php require_once('includes/requeridos.php'); ?>
 
