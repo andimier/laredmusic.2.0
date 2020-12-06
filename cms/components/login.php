90334d5798964e5928f836fd7efc2e9f30e369b9
Andres <andimier@gmail.com>
2020-12-05 19:39:38 -0500

    BE - adjust login functionality

--- cms/components/login.php
+++ cms/components/login.php
@@ -55,13 +55,6 @@
 		return sha1($password);
 	}
 
-
-
-
-
-
-
-
 	if (!isset($_POST['submit'])) {
 		if (isset($_GET['logout']) && $_GET['logout'] == 1) {
 			$mensaje = "Has cerrado tu sesión. ";
@@ -69,9 +62,13 @@
 
 		$username = "";
 		$password = "";
+
+		return;
 	}
 
-	if (!empty(getErrors())) {
+	$errors = getErrors();
+
+	if (!empty($errors)) {
 		$mensaje2 = "Por favor ingresa los siguientes campos:<br />";
 
 		foreach ($errors as $error) {
@@ -83,22 +80,24 @@
 		} else {
 			$mensaje4 = "Hubo " . count($errors) . " errors en el formulario.<br /><br />" ;
 		}
-	} else {
-		$username = trim(mysql_prep($_POST['usuario']));
-		$password = trim(mysql_prep($_POST['contrasena']));
-		$hashed_password = getHash($password);
-		$user = getUser($username, $hashed_password);
 
-		if ($user == null) {
-			$mensaje1 = "El nombre de usuario o contraseña pueden estar errados.";
-		} else {
-			$usuario_encontrado = phpMethods('fetch-array',$user);
-			$_SESSION['user_id'] = $usuario_encontrado['id'];
-			$_SESSION['username'] = $usuario_encontrado['username'];
-
-			header("Location: noticias.php");
-			exit();
-		}
+		return;
 	}
 
+	$username = trim(mysql_prep($_POST['usuario']));
+	$password = trim(mysql_prep($_POST['contrasena']));
+	$hashed_password = getHash($password);
+	$user = getUser($username, $hashed_password);
+
+	if ($user == null) {
+		$mensaje1 = "El nombre de usuario o contraseña pueden estar errados.";
+		return;
+	}
+
+	$usuario_encontrado = phpMethods('fetch-array',$user);
+	$_SESSION['user_id'] = $usuario_encontrado['id'];
+	$_SESSION['username'] = $usuario_encontrado['username'];
+
+	header("Location: noticias.php");
+	exit();
 ?>
