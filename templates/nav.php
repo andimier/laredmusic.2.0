f11f9ea22e471aeec522ec97ea18e42c028346a9
Andres <andimier@gmail.com>
2020-12-22 21:24:58 -0500

    FE - change MM nav links order to reflect content

--- templates/nav.php
+++ templates/nav.php
@@ -1,10 +1,10 @@
 <nav>
     <ul class="nav music-masters-sub-nav">
         <li class="nav-item music-masters-sub-nav-item" href="mm-videos">
-            <a href="#mm-videos"> Videos</a>
+            <a href="#mm-tik-tok">Tik Tok</a>
         </li>
         <li class="nav-item music-masters-sub-nav-item" href="mm-tik-tok">
-            <a href="#mm-tik-tok">Tik Tok</a>
+            <a href="#mm-videos"> Videos</a>
         </li>
         <li class="nav-item music-masters-sub-nav-item" href="mm-noticias">
             <a href="#mm-noticias">Noticias</a>
